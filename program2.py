eedc56df77b807d6c2760a4400dad0861c5ff883
sathwikha yadav <kssathwikha1259@gmail.com>
2024-11-11 16:06:05 +0530

    solution commited

--- program2.py
+++ program2.py
@@ -4,7 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
-
+        if not s:  # Check if the string is empty
+            return 0
+    
+        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        
+        for i in range(len(s) - 1):
+            if roman_map[s[i]] < roman_map[s[i + 1]]:
+                result -= roman_map[s[i]]
+            else:
+                result += roman_map[s[i]]
+        
+        result += roman_map[s[-1]]
+        return result
 
 
